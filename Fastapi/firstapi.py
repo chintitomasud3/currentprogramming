df6a1a24b5cbbbd79ae85892e60f94bb7f77d709
chintitomasud3 <mrahman021992@gmail.com>
2024-02-22 12:14:26 +0600

    hmm

--- Fastapi/firstapi.py
+++ Fastapi/firstapi.py
@@ -7,6 +7,16 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World haire"}
 
+@app.get("/about/{designation}")
+async def about(designation):
+    return {"message":f"we are the best bank in bangladesh i am {designation}"}
 
-
+@app.get("/dpost/{dpost}/{comment}")
+async def dpost(dpost,comment):
+    
+   # return {"message":f"your postname is {dpost} "}
+    return {"messages":{
+         "comment":f"your post name is {dpost}",
+          "custome":f"I am your boss of {comment}"
+    }}
 
